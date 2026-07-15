11f1f5fb74ff38035fc50ee2045dcac9c1d0d033
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-16 02:49:22 +0530

    Add: count-subsequences - submission-4

---
+++ Data Structures & Algorithms/count-subsequences/submission-4.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        // Map<String, Integer> dp = new HashMap<>();
+        int[][] dp = new int[s.length()][t.length()];
+        for(int[] i : dp) {
+            Arrays.fill(i, -1);
+        }
+        
+        return dfs(0, 0, s, t, dp);
+    }
+
+    int dfs(int i, int j, String s, String t, int[][] dp) {
+        if (j == t.length()) {
+            return 1;
+        }
+        if (i == s.length()) {
+            return 0;
+        }
+
+        // String key = "i=" + i + "j=" + j;
+
+        // if (dp.containsKey(key)) {
+        //     return dp.get(key);
+        // }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int count = 0;
+        count += dfs(i + 1, j, s, t, dp);
+        
+        if (s.charAt(i) == t.charAt(j)) {
+            count += dfs(i + 1, j + 1, s, t, dp);
+        }
+
+        // dp.put(key, count);
+        dp[i][j] = count;
+
+        return count;
+    }
+}
