72ea0773df56605f7a55c876b08470fe3f18eb98
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-16 03:12:57 +0530

    Add: count-subsequences - submission-5

---
+++ Data Structures & Algorithms/count-subsequences/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int ROWS = s.length();
+        int COLS = t.length();
+
+        int[][] dp = new int[ROWS + 1][COLS + 1];
+
+        for (int i = 0; i < ROWS; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= ROWS; i++) {
+            for (int j = 1; j <= COLS; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else{
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[ROWS][COLS];
+    }
+}
