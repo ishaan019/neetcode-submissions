6a726af790b5da08a6121956d6e9cff007dc8f27
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-25 16:46:38 +0530

    Add: sql-customers-without-orders - submission-0

---
+++ SQL/sql-customers-without-orders/submission-0.sql
@@ -0,0 +1,9 @@
+-- Write your query below
+-- SELECT NAME FROM T1 FROM T1 LEFT JOIN T2 ON T1.ID = T2.customer_id
+
+SELECT T1.name
+FROM customers AS T1
+LEFT JOIN
+orders AS T2
+ON T1.id = T2.customer_id
+WHERE T2.id IS NULL;
