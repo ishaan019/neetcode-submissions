9863c71f8af1d4c1cfb97095a17b78183f49d5f9
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-26 13:39:27 +0530

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int swimInWater(int[][] grid) {
+        int N = grid.length;
+        boolean[][] visit = new boolean[N][N];
+        // PriorityQueue<int[]> minHeap = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        minHeap.offer(new int[] {grid[0][0], 0, 0});
+        visit[0][0] = true;
+
+        while (!minHeap.isEmpty()) {
+            int[] curr = minHeap.poll();
+            int t = curr[0], r = curr[1], c = curr[2];
+            if (r == N - 1 && c == N - 1) {
+                return t;
+            }
+            for (int[] dir : directions) {
+                int nr = r + dir[0], nc = c + dir[1];
+                if (nr >= 0 && nc >= 0 && nr < N && nc < N && !visit[nr][nc]) {
+                    visit[nr][nc] = true;
+                    minHeap.offer(new int[] {Math.max(t, grid[nr][nc]), nr, nc});
+                }
+            }
+        }
+        return -1;
+    }
+}
