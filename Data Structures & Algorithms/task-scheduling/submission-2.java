09f6357c754238b60ea37cdb6e715ea777891d34
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-27 01:24:14 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.java
@@ -0,0 +1,42 @@
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] arr = new int[26];
+
+        for (char c : tasks) {
+            arr[c - 'A']++;
+        }
+
+        PriorityQueue<Integer> maxHeap =
+            new PriorityQueue<>((a, b) -> Integer.compare(b, a)); // {Frequency}
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > 0) {
+                maxHeap.offer(arr[i]);
+            }
+        }
+
+        int time = 0;
+        Queue<int[]> q = new ArrayDeque<>();// {Frequency, Time at which it would be available again}
+        while (!maxHeap.isEmpty() || !q.isEmpty()) {
+            time++;
+
+            if(maxHeap.isEmpty()) {
+                time = q.peek()[1];
+            } else {
+                int curr = maxHeap.poll();
+                int fr = curr - 1;
+
+                if (fr > 0) {
+                    q.offer(new int[] {fr, time + n});
+                }
+            }
+
+            if(!q.isEmpty() && q.peek()[1] == time) {
+                maxHeap.add(q.poll()[0]);
+            }
+        }
+
+        return time;
+    }
+}
