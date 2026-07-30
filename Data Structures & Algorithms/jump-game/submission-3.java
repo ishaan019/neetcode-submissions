9b4992106926b2070544f1d60e5d46111334e9a2
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-07-31 01:57:16 +0530

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+
+        int N = nums.length;
+        boolean[] dp = new boolean[N];
+
+        dp[N - 1] = true;
+
+        for(int i = N - 2; i >= 0; i--) {
+            if(i + nums[i] >= N - 1) {
+                dp[i] = true;
+            }
+
+            for(int j = i; j <= i + nums[i]; j++) {
+                if(dp[j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
