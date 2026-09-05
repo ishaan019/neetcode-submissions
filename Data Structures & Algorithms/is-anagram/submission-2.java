4684880a31102cb3737f8cbc0a00a9789a825d9f
ishaan019 <69400221+ishaan019@users.noreply.github.com>
2026-09-05 16:06:46 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        int[] map = new int[26];
+
+        for(char ch : s.toCharArray()) {
+            map[ch - 'a']++;
+        }
+
+        for(char ch : t.toCharArray()) {
+            map[ch - 'a']--;
+        }
+
+        for(int i = 0; i < map.length; i++) {
+            if(map[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
